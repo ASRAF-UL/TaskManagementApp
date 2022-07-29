fae11677aed934c877b2e719debd38c34ced1ab4
ASRAF-UL <asraful8625@gmail.com>
2022-07-30 03:48:47 +0600

    Persitent the store data using redux-persist added.

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "../reducer/loginReducer";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+const persistedLoginReducer = persistReducer(persistConfig, loginReducer);
 
 export const store = configureStore({
-  reducer: {
-    auth: loginReducer,
-  },
+  reducer: { auth: persistedLoginReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 export default store;
