5ffc3b01ba4625c7609295fec59bf75c2aea15cb
ASRAF-UL <asraful8625@gmail.com>
2022-07-30 17:20:58 +0600

    Dashboard title button added.

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { logout } from "../../redux/reducer/loginReducer";
+import "./index.css";
 
 function Dashboard() {
-  const dispatch = useDispatch();
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>This is dashboard</h1>
-      <button onClick={() => dispatch(logout())}>Logout</button>
+    <div className="container">
+      <h1>Welcome To Task Manager App</h1>
+      <div className="title__button__div">
+        <button
+          className="task__title__button"
+          onClick={() => navigate("/tasks")}
+        >
+          Tasks
+        </button>
+        <button
+          className="member__title__button"
+          onClick={() => navigate("/members")}
+        >
+          Members
+        </button>
+      </div>
     </div>
   );
 }
